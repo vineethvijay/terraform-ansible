0dcb5863456d33bc74539e7ba603eb4b7545bc0d
Vineeth Vijayan <vineeth.vijayan@lunatech.nl>
2019-07-19 16:02:32 +0200

    cleanup

--- README.md
+++ README.md
@@ -5,8 +5,7 @@
 1. Install the following locally:
     * Terraform
     * Terraform Inventory
-    * Python
-    * pip
+    * Anisble (2.4+)
 2. Set up AWS credentials in [`~/.aws/credentials`]
 3. Ensure you have an SSH public key at `~/.ssh/id_rsa.pub`
 
@@ -14,7 +13,6 @@
 
 ```sh
 export AWS_DEFAULT_REGION=us-east-1
-pip install -r requirements.txt
 
 ./deploy.sh
 ```
--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,9 @@ terraform init
 terraform apply -auto-approve
 
 cd ../ansible
-# pull the instance information from Terraform, and run the Ansible playbook against it to configure
-#TF_STATE=../terraform/terraform.tfstate ansible-playbook "--inventory-file=$(which terraform-inventory)" provision-nginx.yml
 
 #TF_STATE=../terraform/terraform.tfstate ansible-playbook "--inventory-file=$(which terraform-inventory)" provision-tomcat.yml
 #echo "Tomcat setup Success !"
-
 
 #TF_STATE=../terraform/terraform.tfstate ansible-playbook "--inventory-file=$(which terraform-inventory)" provision-nginx.yml
 #echo "Nginx proxy setup Success!"
